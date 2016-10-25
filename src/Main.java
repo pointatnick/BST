c0038c092d0346c297d1acd2ea90c4d1f34de584
pointatnick <nicholashma@gmail.com>
2016-10-25 15:41:37 -0700

    Implemented in-order traversal and added check for insertion of value already in tree

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
-    public static void main(String[] args) {
-        Node root = new Node(10);
-        root.insert(14);
-        root.insert(4);
-    }
+  public static void main(String[] args) {
+    Node root = new Node(5);
+    root.insert(9);
+    root.insert(4);
+    root.insert(1);
+    root.insert(7);
+    root.insert(2);
+    root.insert(8);
+    root.insert(3);
+    root.insert(0);
+    root.insert(6);
+    root.traverse(); // should print out 0-9 in order
+  }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,31 +1,58 @@
 public class Node {
-    private Node left;
-    private Node right;
-    private int val;
+  private Node left;
+  private Node right;
+  private int key;
 
-    public Node(int val) {
-        left = null;
-        right = null;
-        this.val = val;
+  public Node(int n) {
+    left = null;
+    right = null;
+    key = n;
+  }
+
+  public void insert(int n) {
+    if (n < key) {
+      if (left == null) {
+        left = new Node(n);
+      } else {
+        left.insert(n);
+      }
+    } else if (n > key){
+      if (right == null) {
+        right = new Node(n);
+      } else {
+        right.insert(n);
+      }
+    } else {
+      System.out.println(n + " is already in the tree.");
     }
+  }
 
-    public void insert(int val) {
-        if (val < this.val) {
-            if (left == null) {
-                left = new Node(val);
-            } else {
-                left.insert(val);
-            }
-        } else {
-            if (right == null) {
-                right = new Node(val);
-            } else {
-                right.insert(val);
-            }
-        }
+  public void delete(int n) {
+
+  }
+
+  public void search(int n) throws NullPointerException {
+    if (left.key == n || right.key == n) {
+      System.out.println("found " + n);
+    } else {
+      if (left != null) {
+        left.search(n);
+      }
+      if (right != null) {
+        right.search(n);
+      }
     }
+    System.out.println(n + " not found");
+  }
 
-    public void delete(int val) {
-
+  public void traverse() {
+    if (left != null) {
+      left.traverse();
     }
+    System.out.println(key);
+    if (right != null) {
+      right.traverse();
+    }
+  }
+
 }
